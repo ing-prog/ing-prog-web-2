007b2adb9cf090428c5cb3c092ddd7d5766a701c
akenoq <akenoq@yandex.ru>
2018-11-26 17:49:41 +0300

    Урок 1

---
+++ urok_1/code.js
@@ -0,0 +1,18 @@
+"use strict";
+
+window.onload = function () {
+    let f1 = document.getElementById("field1");
+    let f2 = document.getElementById("field2");
+    let b1 = document.getElementById("btn1");
+    let resBox = document.getElementById("result");
+
+    b1.onclick = function () {
+        let a = f1.value;
+        let b = f2.value;
+        a = parseInt(a);
+        b = parseInt(b);
+        let sum = a + b;
+        // alert(sum);
+        resBox.innerHTML = sum;
+    }
+};
