2b638e99e1ecc05b82a611307d3602de7735362c
akenoq <akenoq@yandex.ru>
2018-12-17 23:35:34 +0300

    Урок 4: площадь, обьем, простое, обьекты, генератор обьектов

--- urok_4/prog2.js
+++ urok_4/prog2.js
@@ -1,20 +1,12 @@
 "use strict";
 
-let otrezok = {
-    x1: 5,
-    x2: 16,
-    y1: 3,
-    y2: 15
-};
-
-// object
-console.log(otrezok);
-
-// JSON-object
-console.log(JSON.stringify(otrezok));
-
-// (x1, y1)
-console.log(otrezok.x1 + "; " + otrezok.y1);
+/**
+ * Функция печати - удобная обертка над console.log
+ * @param stroka
+ */
+function print(stroka) {
+    console.log(stroka);
+}
 
 /**
  * Случайное число в интервале от min до max
@@ -26,29 +18,42 @@ function getRandom(min, max) {
     return Math.floor(Math.random() * (max - min + 1)) + min;
 }
 
-function createOtrezok(x1,y1,x2,y2) {
-    let obj = {};
-    obj.x1 = x1;
-    obj.x2 = x2;
-    obj.y1 = y1;
-    obj.y2 = y2;
-    return obj;
+function legthOfOtrezok(otr) {
+    let length = Math.sqrt(
+        Math.pow((otr.x1 - otr.x2), 2) + Math.pow((otr.y1 - otr.y2), 2)
+    );
+    return length;
 }
 
-let new_otrezok_1 = createOtrezok(getRandom(-10,10), getRandom(-10,10),
-                                getRandom(-10,10), getRandom(-10,10));
+let otrezok = {
+    x1: 1,
+    y1: 5,
+    x2: 1,
+    y2: 12
+};
 
-console.log(new_otrezok_1);
+print(legthOfOtrezok(otrezok)); // 7
 
-let new_otrezok_2 = createOtrezok(getRandom(-10,10), getRandom(-10,10),
+print(otrezok);
+print(JSON.stringify(otrezok));
+print(otrezok.x1 + " " + otrezok.y1);
+
+function createOtrezok(x1,y1,x2,y2) {
+    let emptyOtrezok = {};
+
+    emptyOtrezok.x1 = x1;
+    emptyOtrezok.y1 = y1;
+    emptyOtrezok.x2 = x2;
+    emptyOtrezok.y2 = y2;
+
+    emptyOtrezok.len = legthOfOtrezok(emptyOtrezok);
+
+    return emptyOtrezok;
+}
+
+let newOtrezok_1 = createOtrezok(getRandom(-10,10), getRandom(-10,10),
     getRandom(-10,10), getRandom(-10,10));
-
-console.log(new_otrezok_2);
-
-// ДЗ: создать массив отрезков
-// След урок: сервер
-
-
-
-
-
+let newOtrezok_2 = createOtrezok(getRandom(-10,10), getRandom(-10,10),
+    getRandom(-10,10), getRandom(-10,10));
+print(newOtrezok_1);
+print(newOtrezok_2);
